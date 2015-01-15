d0cb046c658c3b892dfbd3bee8e47f8a3b6566dd
Brian Czapiga <rys@evool.com>
2015-01-14 21:22:06 -0500

    Missed a dollar sign

--- defish-rectilinear.sh
+++ defish-rectilinear.sh
@@ -12,7 +12,7 @@ function rectilinear {
     while read filename ; do
         echo Defishing: $filename
         echo "p f0 w${WIDTH}% h${HEIGHT} v115.702 n\"JPEG\"" > defish.pto
-        echo "o w${WIDTH} h{HEIGHT} f2 v140 y0 p0 r0 n${filename}" >> defish.pto
+        echo "o w${WIDTH} h${HEIGHT} f2 v140 y0 p0 r0 n${filename}" >> defish.pto
         echo "m i0" >> defish.pto
         /Applications/Hugin/HuginTools/nona -o rectilinear/$filename defish.pto
         rm -f defish.pto
